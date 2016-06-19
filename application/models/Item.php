carfmccone/POS_O

commit 6cc67ca6884ce99e07bf9d78a0b7754a065ed042

Don't allow adding deleted Items by item_id in Sales register and Receiving (#664)

application/models/Item.php
@@ -4,10 +4,11 @@ class Item extends CI_Model
 	/*
 	Determines if a given item_id is an item
 	*/
-	public function exists($item_id)
+	public function exists($item_id, $deleted = FALSE)
 	{
 		$this->db->from('items');
 		$this->db->where('item_id', $item_id);
+		$this->db->where('deleted', $deleted);
 
 		return ($this->db->get()->num_rows() == 1);
 	}

commit 68e05b5cc54d3adcd38f602a81f7614ae9821e30

Fix regression introduced with #664 around deleted items (#672)

application/models/Item.php
@@ -226,7 +226,7 @@ class Item extends CI_Model
 	*/
 	public function save(&$item_data, $item_id = FALSE)
 	{
-		if(!$item_id || !$this->exists($item_id))
+		if(!$item_id || !$this->exists($item_id, TRUE))
 		{
 			if($this->db->insert('items', $item_data))
 			{
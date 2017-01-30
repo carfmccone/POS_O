Darli963/sistema-inventario-ventas

Changes to support Item Kits, support for MySQL 5.7, and a tweak for PHP 7

application/models/Item.php
@@ -60,13 +60,58 @@ class Item extends CI_Model
 	*/
 	public function search($search, $filters, $rows = 0, $limit_from = 0, $sort = 'items.name', $order = 'asc')
 	{
-		$this->db->from('items');
-		$this->db->join('suppliers', 'suppliers.person_id = items.supplier_id', 'left');
-		$this->db->join('inventory', 'inventory.trans_items = items.item_id');
+		$this->db->select('max(items.name) as name');
+		$this->db->select('max(items.category) as category');
+		$this->db->select('max(items.supplier_id) as supplier_id');
+		$this->db->select('max(items.item_number) as item_number');
+		$this->db->select('max(items.description) as description');
+		$this->db->select('max(items.cost_price) as cost_price');
+		$this->db->select('max(items.unit_price) as unit_price');
+		$this->db->select('max(items.reorder_level) as reorder_level');
+		$this->db->select('max(items.receiving_quantity) as receiving_quantity');
+		$this->db->select('items.item_id as item_id');
+		$this->db->select('max(items.pic_id) as pic_id');
+		$this->db->select('max(items.allow_alt_description) as allow_alt_description');
+		$this->db->select('max(items.is_serialized) as is_serialized');
+		$this->db->select('max(items.deleted) as deleted');
+		$this->db->select('max(items.custom1) as custom1');
+		$this->db->select('max(items.custom2) as custom2');
+		$this->db->select('max(items.custom3) as custom3');
+		$this->db->select('max(items.custom4) as custom4');
+		$this->db->select('max(items.custom5) as custom5');
+		$this->db->select('max(items.custom6) as custom6');
+		$this->db->select('max(items.custom7) as custom7');
+		$this->db->select('max(items.custom8) as custom8');
+		$this->db->select('max(items.custom9) as custom9');
+		$this->db->select('max(items.custom10) as custom10');
+
+		$this->db->select('max(suppliers.person_id) as person_id');
+		$this->db->select('max(suppliers.company_name) as company_name');
+		$this->db->select('max(suppliers.agency_name) as agency_name');
+		$this->db->select('max(suppliers.account_number) as account_number');
+		$this->db->select('max(suppliers.deleted) as deleted');
+
+		$this->db->select('max(inventory.trans_id) as trans_id');
+		$this->db->select('max(inventory.trans_items) as trans_items');
+		$this->db->select('max(inventory.trans_user) as trans_user');
+		$this->db->select('max(inventory.trans_date) as trans_date');
+		$this->db->select('max(inventory.trans_comment) as trans_comment');
+		$this->db->select('max(inventory.trans_location) as trans_location');
+		$this->db->select('max(inventory.trans_inventory) as trans_inventory');
+
+		if($filters['stock_location_id'] > -1) {
+			$this->db->select('max(item_quantities.item_id) as qty_item_id');
+			$this->db->select('max(item_quantities.location_id) as location_id');
+			$this->db->select('sum(item_quantities.quantity) as quantity');
+		}
+
+		$this->db->from('items as items');
+		$this->db->join('suppliers as suppliers', 'suppliers.person_id = items.supplier_id', 'left');
+		$this->db->join('inventory as inventory', 'inventory.trans_items = items.item_id');
 
 		if($filters['stock_location_id'] > -1)
 		{
-			$this->db->join('item_quantities', 'item_quantities.item_id = items.item_id');
+			$this->db->join('item_quantities as item_quantities', 'item_quantities.item_id = items.item_id');
 			$this->db->where('location_id', $filters['stock_location_id']);
 		}
 
@@ -122,18 +167,18 @@ class Item extends CI_Model
 
 		// avoid duplicated entries with same name because of inventory reporting multiple changes on the same item in the same date range
 		$this->db->group_by('items.item_id');
-		
+
 		// order by name of item
 		$this->db->order_by($sort, $order);
 
-		if($rows > 0) 
-		{	
+		if($rows > 0)
+		{
 			$this->db->limit($rows, $limit_from);
 		}
 
 		return $this->db->get();
 	}
-	
+
 	/*
 	Returns all the items
 	*/
@@ -192,7 +237,7 @@ class Item extends CI_Model
 			return $item_obj;
 		}
 	}
-	
+
 	/*
 	Gets information about a particular item by item id or number
 	*/
@@ -200,17 +245,17 @@ class Item extends CI_Model
 	{
 		$this->db->from('items');
 
-        if (ctype_digit($item_id))
-        {
-            $this->db->group_start();
-                $this->db->where('item_id', (int) $item_id);
-                $this->db->or_where('items.item_number', $item_id);
-            $this->db->group_end();
-        }
-        else
-        {
-            $this->db->where('item_number', $item_id);
-        }
+		if (ctype_digit($item_id))
+		{
+			$this->db->group_start();
+				$this->db->where('item_id', (int) $item_id);
+				$this->db->or_where('items.item_number', $item_id);
+			$this->db->group_end();
+		}
+		else
+		{
+			$this->db->where('item_number', $item_id);
+		}
 
 		$this->db->where('items.deleted', 0);
 
@@ -236,7 +281,7 @@ class Item extends CI_Model
 		{
 			$this->db->where('items.deleted', $deleted);
 		}
-        
+
 		$query = $this->db->get();
 
 		if($query->num_rows() == 1)
@@ -277,7 +322,7 @@ class Item extends CI_Model
 
 			return FALSE;
 		}
-		
+
 		$this->db->where('item_id', $item_id);
 
 		return $this->db->update('items', $item_data);
@@ -305,14 +350,14 @@ class Item extends CI_Model
 		$this->Item_quantity->reset_quantity($item_id);
 		$this->db->where('item_id', $item_id);
 		$success = $this->db->update('items', array('deleted'=>1));
-		
+
 		$this->db->trans_complete();
-		
+
 		$success &= $this->db->trans_status();
 
 		return $success;
 	}
-	
+
 	/*
 	Undeletes one item
 	*/
@@ -335,13 +380,13 @@ class Item extends CI_Model
 		$this->Item_quantity->reset_quantity_list($item_ids);
 		$this->db->where_in('item_id', $item_ids);
 		$success = $this->db->update('items', array('deleted'=>1));
-		
+
 		$this->db->trans_complete();
-		
+
 		$success &= $this->db->trans_status();
 
 		return $success;
- 	}
+	}
 
 	public function get_search_suggestions($search, $filters = array('is_deleted' => FALSE, 'search_custom' => FALSE), $unique = FALSE, $limit = 25)
 	{
@@ -350,6 +395,7 @@ class Item extends CI_Model
 		$this->db->select('item_id, name');
 		$this->db->from('items');
 		$this->db->where('deleted', $filters['is_deleted']);
+		$this->db->where("item_type = '0'"); // standard, exclude kit items since kits will be picked up later
 		$this->db->like('name', $search);
 		$this->db->order_by('name', 'asc');
 		foreach($this->db->get()->result() as $row)
@@ -458,7 +504,7 @@ class Item extends CI_Model
 
 		return $suggestions;
 	}
-	
+
 	public function get_location_suggestions($search)
 	{
 		$suggestions = array();
@@ -472,7 +518,7 @@ class Item extends CI_Model
 		{
 			$suggestions[] = array('label' => $row->location);
 		}
-	
+
 		return $suggestions;
 	}
 
@@ -490,7 +536,7 @@ class Item extends CI_Model
 			$row_array = (array) $row;
 			$suggestions[] = array('label' => $row_array['custom'.$field_no]);
 		}
-	
+
 		return $suggestions;
 	}
 

carfmccone/POS_O

application/models/Item.php
@@ -60,13 +60,58 @@ class Item extends CI_Model
 	*/
 	public function search($search, $filters, $rows = 0, $limit_from = 0, $sort = 'items.name', $order = 'asc')
 	{
-		$this->db->from('items');
-		$this->db->join('suppliers', 'suppliers.person_id = items.supplier_id', 'left');
-		$this->db->join('inventory', 'inventory.trans_items = items.item_id');
+		$this->db->select('max(items.name) as name');
+		$this->db->select('max(items.category) as category');
+		$this->db->select('max(items.supplier_id) as supplier_id');
+		$this->db->select('max(items.item_number) as item_number');
+		$this->db->select('max(items.description) as description');
+		$this->db->select('max(items.cost_price) as cost_price');
+		$this->db->select('max(items.unit_price) as unit_price');
+		$this->db->select('max(items.reorder_level) as reorder_level');
+		$this->db->select('max(items.receiving_quantity) as receiving_quantity');
+		$this->db->select('items.item_id as item_id');
+		$this->db->select('max(items.pic_id) as pic_id');
+		$this->db->select('max(items.allow_alt_description) as allow_alt_description');
+		$this->db->select('max(items.is_serialized) as is_serialized');
+		$this->db->select('max(items.deleted) as deleted');
+		$this->db->select('max(items.custom1) as custom1');
+		$this->db->select('max(items.custom2) as custom2');
+		$this->db->select('max(items.custom3) as custom3');
+		$this->db->select('max(items.custom4) as custom4');
+		$this->db->select('max(items.custom5) as custom5');
+		$this->db->select('max(items.custom6) as custom6');
+		$this->db->select('max(items.custom7) as custom7');
+		$this->db->select('max(items.custom8) as custom8');
+		$this->db->select('max(items.custom9) as custom9');
+		$this->db->select('max(items.custom10) as custom10');
+
+		$this->db->select('max(suppliers.person_id) as person_id');
+		$this->db->select('max(suppliers.company_name) as company_name');
+		$this->db->select('max(suppliers.agency_name) as agency_name');
+		$this->db->select('max(suppliers.account_number) as account_number');
+		$this->db->select('max(suppliers.deleted) as deleted');
+
+		$this->db->select('max(inventory.trans_id) as trans_id');
+		$this->db->select('max(inventory.trans_items) as trans_items');
+		$this->db->select('max(inventory.trans_user) as trans_user');
+		$this->db->select('max(inventory.trans_date) as trans_date');
+		$this->db->select('max(inventory.trans_comment) as trans_comment');
+		$this->db->select('max(inventory.trans_location) as trans_location');
+		$this->db->select('max(inventory.trans_inventory) as trans_inventory');
+
+		if($filters['stock_location_id'] > -1) {
+			$this->db->select('max(item_quantities.item_id) as qty_item_id');
+			$this->db->select('max(item_quantities.location_id) as location_id');
+			$this->db->select('max(item_quantities.quantity) as quantity');
+		}
+
+		$this->db->from('items as items');
+		$this->db->join('suppliers as suppliers', 'suppliers.person_id = items.supplier_id', 'left');
+		$this->db->join('inventory as inventory', 'inventory.trans_items = items.item_id');
 
 		if($filters['stock_location_id'] > -1)
 		{
-			$this->db->join('item_quantities', 'item_quantities.item_id = items.item_id');
+			$this->db->join('item_quantities as item_quantities', 'item_quantities.item_id = items.item_id');
 			$this->db->where('location_id', $filters['stock_location_id']);
 		}
 
@@ -129,18 +174,18 @@ class Item extends CI_Model
 
 		// avoid duplicated entries with same name because of inventory reporting multiple changes on the same item in the same date range
 		$this->db->group_by('items.item_id');
-		
+
 		// order by name of item
 		$this->db->order_by($sort, $order);
 
-		if($rows > 0) 
-		{	
+		if($rows > 0)
+		{
 			$this->db->limit($rows, $limit_from);
 		}
 
 		return $this->db->get();
 	}
-	
+
 	/*
 	Returns all the items
 	*/
@@ -199,7 +244,7 @@ class Item extends CI_Model
 			return $item_obj;
 		}
 	}
-	
+
 	/*
 	Gets information about a particular item by item id or number
 	*/
@@ -207,17 +252,17 @@ class Item extends CI_Model
 	{
 		$this->db->from('items');
 
-        if (ctype_digit($item_id))
-        {
-            $this->db->group_start();
-                $this->db->where('item_id', (int) $item_id);
-                $this->db->or_where('items.item_number', $item_id);
-            $this->db->group_end();
-        }
-        else
-        {
-            $this->db->where('item_number', $item_id);
-        }
+		if (ctype_digit($item_id))
+		{
+			$this->db->group_start();
+				$this->db->where('item_id', (int) $item_id);
+				$this->db->or_where('items.item_number', $item_id);
+			$this->db->group_end();
+		}
+		else
+		{
+			$this->db->where('item_number', $item_id);
+		}
 
 		$this->db->where('items.deleted', 0);
 
@@ -243,7 +288,7 @@ class Item extends CI_Model
 		{
 			$this->db->where('items.deleted', $deleted);
 		}
-        
+
 		$query = $this->db->get();
 
 		if($query->num_rows() == 1)
@@ -284,7 +329,7 @@ class Item extends CI_Model
 
 			return FALSE;
 		}
-		
+
 		$this->db->where('item_id', $item_id);
 
 		return $this->db->update('items', $item_data);
@@ -312,14 +357,14 @@ class Item extends CI_Model
 		$this->Item_quantity->reset_quantity($item_id);
 		$this->db->where('item_id', $item_id);
 		$success = $this->db->update('items', array('deleted'=>1));
-		
+
 		$this->db->trans_complete();
-		
+
 		$success &= $this->db->trans_status();
 
 		return $success;
 	}
-	
+
 	/*
 	Undeletes one item
 	*/
@@ -342,13 +387,13 @@ class Item extends CI_Model
 		$this->Item_quantity->reset_quantity_list($item_ids);
 		$this->db->where_in('item_id', $item_ids);
 		$success = $this->db->update('items', array('deleted'=>1));
-		
+
 		$this->db->trans_complete();
-		
+
 		$success &= $this->db->trans_status();
 
 		return $success;
- 	}
+	}
 
 	public function get_search_suggestions($search, $filters = array('is_deleted' => FALSE, 'search_custom' => FALSE), $unique = FALSE, $limit = 25)
 	{
@@ -357,6 +402,7 @@ class Item extends CI_Model
 		$this->db->select('item_id, name');
 		$this->db->from('items');
 		$this->db->where('deleted', $filters['is_deleted']);
+		$this->db->where("item_type = '0'"); // standard, exclude kit items since kits will be picked up later
 		$this->db->like('name', $search);
 		$this->db->order_by('name', 'asc');
 		foreach($this->db->get()->result() as $row)
@@ -465,7 +511,7 @@ class Item extends CI_Model
 
 		return $suggestions;
 	}
-	
+
 	public function get_location_suggestions($search)
 	{
 		$suggestions = array();
@@ -479,7 +525,7 @@ class Item extends CI_Model
 		{
 			$suggestions[] = array('label' => $row->location);
 		}
-	
+
 		return $suggestions;
 	}
 
@@ -497,7 +543,7 @@ class Item extends CI_Model
 			$row_array = (array) $row;
 			$suggestions[] = array('label' => $row_array['custom'.$field_no]);
 		}
-	
+
 		return $suggestions;
 	}
 